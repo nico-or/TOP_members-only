e66a014da3d6435a47dd7fc0dceafa0c8b87ed11
Nicolás Órdenes <nicordenes@gmail.com>
2022-05-06 19:03:07 -0400

    Recreate User model with scaffolding

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :users
+
   get 'signup', to: 'user#new', as: 'signup'
   get 'login', to: 'session#new', as: 'login'
   get 'logout', to: 'session#destroy', as: 'logout'
