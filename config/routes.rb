e90e99ad03e8a46878ebef8f704b6ee32c78ac80
Nicolás Órdenes <nicordenes@gmail.com>
2022-05-06 18:35:18 -0400

    Add authentication routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'signup', to: 'user#new', as: 'signup'
+  get 'login', to: 'session#new', as: 'login'
+  get 'logout', to: 'session#destroy', as: 'logout'
+
   root 'site#home'
 end
