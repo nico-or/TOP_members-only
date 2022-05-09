a6fcd0099077391fc92d3d6a31c36210617ab919
Nicolás Órdenes <nicordenes@gmail.com>
2022-05-09 18:01:28 -0400

    Reduce resource routes creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :sessions
+  resources :users, only: %i[new create]
+  resources :sessions, only: %i[new create destroy]
 
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
