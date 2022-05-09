8a4ffbc4e0fad8fa76b9d7243c5560e17e9305db
Nicolás Órdenes <nicordenes@gmail.com>
2022-05-09 19:13:54 -0400

    Add Post index and show partial

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  resources :users, only: %i[new create]
-  resources :sessions, only: %i[new create destroy]
+  root 'posts#index'
 
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
 
-  root 'site#home'
+  resources :posts, only: %i[index new create]
+  resources :users, only: %i[new create]
+  resources :sessions, only: %i[new create destroy]
 end
