209152a9ba439ad2b810a0f97851f70cdd6e1538
Nicolás Órdenes <nicordenes@gmail.com>
2022-05-10 12:16:03 -0400

    Fix User forms, model, views and controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
+  validates_presence_of %i[email first_name last_name]
+  validates_uniqueness_of %i[email]
+
   has_many :posts
 
   def full_name
