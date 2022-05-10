17983fdfd70d407b07e90169c5318f8aec1671de
Nicolás Órdenes <nicordenes@gmail.com>
2022-05-09 22:35:09 -0400

    Add user#full_name method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,6 @@ class SessionsController < ApplicationController
 
     if user && user&.authenticate(params[:password])
       session[:user_id] = user.id
-      session[:user_name] = "#{user.first_name} #{user.last_name}"
       redirect_to root_url, notice: 'Logged in'
     else
       redirect_to login_path, alert: 'Incorrect email or password'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :posts
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
